641eb462ad64d4b7b08589bf8d111263309b64c0
Adi Booker <adib4324@gmail.com>
2024-03-25 16:18:57 +0200

    some challenges takled

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -2,18 +2,20 @@ import { useState, useRef } from "react";
 import TopBar from "./TopBar";
 import GoogleMap from "./GoogleMap";
 
-// utility function
 const log = (...args) => console.log.apply(null, ["App -->", ...args]);
 
 export default function App() {
-
-  const input = useRef(null);
+  const zoomInput = useRef(null);
+  const markerTitleInput = useRef(null);
+  const [markers, setMarkers] = useState([]);
   const [zoom, setZoom] = useState(8);
   const [latlng, setLatlng] = useState({
     lat: -34.397,
     lng: 150.644,
   });
+  const googleMapRef = useRef(null);
 
+  
   function reposition(city) {
     switch (city) {
       case "tel aviv":
@@ -21,46 +23,85 @@ export default function App() {
         break;
         case "london":
           setLatlng({ lat: 51.509865, lng: -0.118092 });
-        break;
-        case "paris":
-          setLatlng({ lat: 48.864716, lng: 2.349014 });
           break;
-        case "uluru":
-          setLatlng({ lat: -25.344, lng: 131.031 });
-        break;
-          default:
-          alert("Location not supported");
-    }
-  }
+          case "paris":
+            setLatlng({ lat: 48.864716, lng: 2.349014 });
+            break;
+            case "uluru":
+              setLatlng({ lat: -25.344, lng: 131.031 });
+              break;
+              default:
+                alert("Location not supported");
+              }
+            }
+            
+            function onZoomChange(){
+              setZoom(Number(zoomInput.current.value))
+            }
 
-  function onZoomChange(){
-    setZoom(Number(input.current.value))
+      function addMarker() {
+        const markerTitle = `Marker (${latlng.lat}, ${latlng.lng})`; 
+        const [lat, lng] = [latlng.lat, latlng.lng]; 
+        console.log("Marker Title:", markerTitle);
+        console.log("Parsed lat:", lat, "Parsed lng:", lng);
+        
+     if (!isNaN(lat) && !isNaN(lng)) {
+        const newMarker = {
+        position: { lat, lng },
+        title: markerTitle,
+      };
+        setMarkers([...markers, newMarker]);
+          if(googleMapRef.current) {
+          googleMapRef.current.addMarker(newMarker);
+      }
+      } else {
+          alert("Invalid latitude and longitude values. Please enter valid values.");
+      }
   }
-
-    log(latlng);
-    return (
-      <div className="app">
-        <TopBar>Google Maps Example in React</TopBar>
-        <div className="hbox mb20">
-          <button onClick={() => reposition("tel aviv")}>Tel Aviv</button>
-          <button onClick={() => reposition("london")}>London</button>
-          <button onClick={() => reposition("paris")}>Paris</button>
-          <button onClick={() => reposition("uluru")}>Uluru</button>
-          <input
-            ref={input}
-            type="number"
-            min="8"
-            max="16"
-            placeholder="8"
-            onChange={ onZoomChange }
-          />
-        </div>
-            <GoogleMap
-             lat={latlng.lat} 
-             lng={latlng.lng}
-             zoom={zoom}
-            />
+    
+  log(latlng);
+  return (
+    <div className="app">
+      <TopBar>Google Maps Example in React</TopBar>
+      <div className="hbox mb20">
+        <button onClick={() => reposition("tel aviv")}>Tel Aviv</button>
+        <button onClick={() => reposition("london")}>London</button>
+        <button onClick={() => reposition("paris")}>Paris</button>
+        <button onClick={() => reposition("uluru")}>Uluru</button>
+        <input
+          ref={zoomInput}
+          type="number"
+          min="8"
+          max="16"
+          placeholder="8"
+          onChange={onZoomChange}
+        />
       </div>
-    );
-  
+      <br />
+      <div className="markerBox">
+        <p>Marker Title: </p>
+        <input ref={markerTitleInput} type="text" placeholder="Marker Title (lat: -34.397, lng: 150.644)" />
+        <select name="Crs" id="cars">
+          <optgroup label="Type">
+            <option value="Null">marker type</option>
+            <option value="saab">Barbecue</option>
+            <option value="Null">Buffet</option>
+            <option value="saab">Brasserie</option>
+            <option value="Null">Cafe</option>
+            <option value="saab">Dinner</option>
+            <option value="Null">Ethnic</option>
+            <option value="saab">Kosher</option>
+          </optgroup>
+        </select>
+        <button onClick={addMarker}>Add Marker</button>
+      </div>
+      <GoogleMap
+        ref={googleMapRef}
+        lat={latlng.lat}
+        lng={latlng.lng}
+        zoom={zoom}
+        addMarker={addMarker}
+      />
+    </div>
+  );
 }
--- src/view/GoogleMap.jsx
+++ src/view/GoogleMap.jsx
@@ -1,32 +1,47 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState, useRef, forwardRef  } from "react";
+
 const log = (...args) => console.log.apply(null, ["GoogleMap -->", ...args]);
 
-export default function GoogleMap({ lat, lng ,zoom}) {
+export default function GoogleMap({ lat, lng, zoom, addMarker }) {
   const map = useRef(null);
   const mapDiv = useRef(null);
-  const markerPosition = { lat: -25.344, lng: 131.031 };
-
-  
-  async function createMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    map.current = new Map(mapDiv.current, {
-      center: { lat, lng },
-      zoom: 8,
-      mapId: "DEMO_MAP_ID"
-    });
-    
-    const marker = new AdvancedMarkerElement({
-     map: map.current,
-     position: markerPosition,
-     title: "Uluru",
-    });
-  }
+  const [cCoordinates, setCCoordinates] = useState({ latC: -1, lngC: 1 });
+  const [markers, setMarkers] = useState([]);
   
   useEffect(() => {
-    createMap();
-  }, []);
+    async function createMap() {
+      console.log("creating map...");
+      const { Map } = await google.maps.importLibrary("maps");
+      console.log("Map library loaded");
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+      console.log("AdvancedMarkerElement library loaded");
+      map.current = new Map(mapDiv.current, {
+        center: { lat, lng },
+        zoom: 8,
+        mapId: 'ea7f33ffc97415b3',
+        addMarker: ""
+      });
+      
+      extractCenterLatlng();
+      
+      const marker1 = new AdvancedMarkerElement({
+        map: map.current,
+        position: { lat: -25.344, lng: 131.031 },
+        title: "Uluru",
+      });
 
+      const marker2 = new AdvancedMarkerElement({
+        map: map.current,
+        position: { lat: 32.0042938, lng: 34.7615399 },
+        title: "Tel-Aviv",
+      });
+      
+      // Set initial markers
+      setMarkers([marker1, marker2]);
+    }
+    createMap();
+  }, [lat, lng]);
+  
   useEffect(() => {
     if(!map.current) return;
     log("useEffect >>>>");
@@ -36,22 +51,26 @@ export default function GoogleMap({ lat, lng ,zoom}) {
     log("mapDiv:", mapDiv);
     log("<<<< useEffect");
     map.current.setCenter({ lat, lng });
-    }, [lat, lng]);
-
-    useEffect(() => {
+  }, [lat, lng]);
+  
+  useEffect(() => {
     if (map.current) {
       map.current.setZoom(zoom);
     }
   }, [zoom]);
-  
-  return <div ref={mapDiv} className="map-box" />;
-  
+
+  const extractCenterLatlng = () => {
+    if (map.current) {
+        map.current.addListener("center_changed", () => {
+          const center = map.current.getCenter();
+          const latC = center.lat();
+          const lngC = center.lng();
+          console.log("Center Latitude:", latC);
+          console.log("Center Longitude:", lngC);
+          setCCoordinates({ latC, lngC });
+        });
+      }
+    };
+
+   return <div ref={mapDiv} className="map-box" />;
 }
-
-
-
-  // useEffect(() => {
-  //   if  (map.current) {
-  //     map.current.;
-  //   }
-  // }, []);
